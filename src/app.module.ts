ff6d105887c736fd672f31176e26287ada04f8ba
Grom Max <MailMaximGrom@gmail.com>
2020-02-29 21:45:37 +0200

    Created todo module and todo entity. Added typeorm to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './modules/todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/todo/entities/todo.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ default: false })
+  isCompleted: boolean;
+}
