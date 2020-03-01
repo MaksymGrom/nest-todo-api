b0c86c420979fabf731d6581f2a5a41aed017a30
Grom Max <MailMaximGrom@gmail.com>
2020-03-01 11:15:09 +0200

    Added Swagger to the project

--- src/modules/todo/controllers/todo.controller.ts
+++ src/modules/todo/controllers/todo.controller.ts
@@ -1,120 +1,16 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get, HttpException, HttpStatus,
-  NotFoundException,
-  Param,
-  Post,
-  Put,
-} from '@nestjs/common';
-import { CreateDto, UpdateDto } from './dto';
+import { Controller } from '@nestjs/common';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../entities/todo.entity';
-import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { NotFoundResponse } from './type';
+import { ApiTags } from '@nestjs/swagger';
+import { Crud, CrudController } from '@nestjsx/crud';
 
+@Crud({
+  model: {
+    type: Todo
+  }
+})
 @ApiTags('todo')
 @Controller('rest/todo')
-export class TodoController {
-  constructor(private readonly todoService: TodoService) {}
-
-  @Get()
-  @ApiResponse({
-    status: 200,
-    description: 'get all todo',
-    type: [Todo],
-  })
-  getAllAction(): Promise<Todo[]> {
-    return this.todoService.findAll();
-  }
-
-  @Get(':id')
-  @ApiResponse({
-    status: 200,
-    description: 'get todo by id',
-    type: Todo,
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Not Found',
-    type: NotFoundResponse
-  })
-  async getOneAction(@Param('id') id: string): Promise<Todo> {
-    const todo = await this.todoService.findOne(id);
-    if (todo === undefined) {
-      throw new HttpException(
-        'Todo with id=' + id + ' not exists',
-        HttpStatus.NOT_FOUND
-      );
-    }
-    return todo;
-  }
-
-  @Post()
-  @ApiResponse({
-    status: 200,
-    description: 'create todo',
-    type: Todo,
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Not Found',
-    type: NotFoundResponse
-  })
-  @ApiBody({ type: CreateDto })
-  createAction(@Body() createDto: CreateDto): Promise<Todo> {
-    const todo = new Todo();
-    todo.title = createDto.title;
-    if (createDto.isCompleted !== undefined) {
-      todo.isCompleted = createDto.isCompleted;
-    }
-    return this.todoService.create(todo);
-  }
-
-  @Put(':id')
-  @ApiResponse({
-    status: 200,
-    description: 'update todo',
-    type: Todo,
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Not Found',
-    type: NotFoundResponse
-  })
-  @ApiBody({ type: UpdateDto })
-  async updateAction(
-    @Param('id') id: string,
-    @Body() {title, isCompleted = false}: UpdateDto
-  ): Promise<Todo> {
-    const todo = await this.todoService.findOne(id);
-    if (todo === undefined) {
-      throw new NotFoundException('Todo with id=' + id + ' not exists');
-    }
-    todo.title = title;
-    todo.isCompleted = isCompleted;
-    return this.todoService.update(todo);
-  }
-
-  @Delete(':id')
-  @ApiResponse({
-    status: 200,
-    description: 'delete todo',
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Not Found',
-    type: NotFoundResponse
-  })
-  async deleteAction(@Param('id') id: string): Promise<{success: boolean}> {
-    const todo = await this.todoService.findOne(id);
-    if (todo === undefined) {
-      throw new NotFoundException('Todo with id=' + id + ' not exists');
-    }
-    await this.todoService.remove(id);
-    return {
-      success: true
-    };
-  }
+export class TodoController implements CrudController<Todo> {
+  constructor(public service: TodoService) {}
 }
--- src/modules/todo/services/todo.service.ts
+++ src/modules/todo/services/todo.service.ts
@@ -1,33 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Todo } from '../entities/todo.entity';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 
 @Injectable()
-export class TodoService {
-  constructor(
-    @InjectRepository(Todo)
-    private readonly todoRepository: Repository<Todo>,
-  ) {}
-
-  findAll(): Promise<Todo[]> {
-    return this.todoRepository.find();
-  }
-
-  findOne(id: string): Promise<Todo> {
-    return this.todoRepository.findOne(id);
-  }
-
-  create(todo: Todo): Promise<Todo> {
-    delete todo.id;
-    return this.todoRepository.save(todo);
-  }
-
-  update(todo: Todo): Promise<Todo> {
-    return this.todoRepository.save(todo);
-  }
-
-  async remove(id: string): Promise<void> {
-    await this.todoRepository.delete(id);
+export class TodoService extends TypeOrmCrudService<Todo> {
+  constructor(@InjectRepository(Todo) todoRepository) {
+    super(todoRepository);
   }
 }
